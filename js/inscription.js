ef82204f5ad7974042528fc9abac731898517c20
Marco <garcia.j.marco@gmail.com>
2017-04-13 10:36:27 +0200

    Peut maintemant fermer menu en cliquand hors de celui ci

--- js/inscription.js
+++ js/inscription.js
@@ -2,21 +2,20 @@
 var menu = document.getElementById("icon");
 var slash = document.getElementsByClassName("slash");
 var sombre = document.getElementById("sombre");
+var classSombre = document.getElementsByClassName("sombre");
 nav.style.right = "100%";
 
 
 document.getElementById("icon").onclick = function () {
-console.log("test")
+    animMenu();
+};
+
+function animMenu() {
 
     if (nav.style.right == "100%") {
-        sombre.classList.add("sombre");
 
         nav.style.right = "70%";
         slash[1].style.opacity = 0;
-
-        slash[0].style.backgroundColor = "black";
-        slash[1].style.backgroundColor = "black";
-        slash[2].style.backgroundColor = "black";
 
         slash[0].style.top = "12px";
         slash[2].style.top = "-12px";
@@ -26,14 +25,9 @@ console.log("test")
 
     }
     else {
-        sombre.classList.remove("sombre");
 
         nav.style.right = "100%";
         slash[1].style.opacity = 1;
-
-        slash[0].style.backgroundColor = "white";
-        slash[1].style.backgroundColor = "white";
-        slash[2].style.backgroundColor = "white";
 
         slash[0].style.top = "0px";
         slash[2].style.top = "0px";
@@ -41,4 +35,17 @@ console.log("test")
         slash[0].style.transform = "rotate(0deg)";
         slash[2].style.transform = "rotate(0deg)";
     }
-}
+
+    sombre.classList.toggle("sombre");
+    slash[0].classList.toggle("colorHamb");
+    slash[1].classList.toggle("colorHamb");
+    slash[2].classList.toggle("colorHamb");
+
+};
+
+
+sombre.onclick = function () {
+    if (classSombre[0]) {
+        animMenu();
+    }
+};

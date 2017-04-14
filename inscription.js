4f23e3d6d5f58cc65dcd3a380f6e1b9d9c7b92e1
fahemuu <fahem.id@gmail.com>
2017-04-14 15:16:45 +0200

    dddff

--- inscription.js
+++ inscription.js
@@ -11,14 +11,14 @@ function sendEmail() {
 
         var mail = 'mailto:justine@wildcodeschool.fr';
         var subject = "subject=Inscription formation Swift/iOS_" + document.getElementById("iNnom").value
-        var my_body = "body=" + document.getElementById("content").value
+        var my_body = document.getElementById("content").value
         var a = document.createElement('a');
         a.href = mail
             + "?" + subject
-            + "&" + "Hello, je souhaite suivre la formation intensive Swift / iOS. Voici mon profil GitHub et/ou Linkedin :"
+            + "&body=" + "Hello, je souhaite suivre la formation intensive Swift / iOS. Voici mon profil GitHub et/ou Linkedin :"+my_body
             + document.getElementById("urlG").value + "\n"
             + document.getElementById("urlk").value
-            + "" + my_body
+            + "" 
 
 
 

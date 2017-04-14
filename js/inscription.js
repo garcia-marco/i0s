52c162b98a1a3efaa982e6bb5de7c83428488194
Marco <garcia.j.marco@gmail.com>
2017-04-14 13:49:53 +0200

    design et améliorations

--- js/inscription.js
+++ js/inscription.js
@@ -1,4 +1,16 @@
 
+
+
+
+// Menu Hamburger
+
+
+
+
+
+
+
+
 var menu = document.getElementById("icon");
 var slash = document.getElementsByClassName("slash");
 var sombre = document.getElementById("sombre");
@@ -44,12 +56,151 @@ function animMenu() {
 
     sombre.classList.toggle("sombre");
 
-    /*  slash[0].classList.toggle("colorSlash");
-      slash[1].classList.toggle("colorSlash");
-      slash[2].classList.toggle("colorSlash"); */
+    slash[0].classList.toggle("colorSlash");
+    slash[1].classList.toggle("colorSlash");
+    slash[2].classList.toggle("colorSlash");
 
-    for (let i in slash) {
-        slash[i].classList.toggle("colorSlash");
-    }
-
+    /* for (let i in slash) {
+        slash[i].classList.toggle("colorSlash"); 
+    } */
 };
+
+
+
+
+
+
+
+
+// Fin Menu Hamburger
+
+
+//Scroll
+var isSmoothScrollSupported = 'scrollBehavior' in document.documentElement.style;
+// Questionnaire
+
+
+
+
+
+// Nombre de Questions
+var nbQuestions = 5;
+
+// Variable pour la note finale
+var grade = 0;
+
+// Initialise les réponses et questions
+var allAnswer = [];
+var allRadios = [];
+
+function init() {
+
+    for (let i = 0; i < nbQuestions; i++) {
+        allAnswer.push(document.getElementById("answer" + i));
+        allRadios.push(document.getElementsByName("radiosQ" + i));
+    }
+};
+init();
+
+
+
+// Se lance si clique sur bouton
+document.getElementById("submit").onclick = function click() {
+
+    if (checking() == nbQuestions) {
+        answerAll();
+    }
+    else {
+        document.getElementById("submit").innerHTML = "Incomplet";
+    }
+};
+
+
+
+// Se lance si tout est checked
+function answerAll() {
+
+    // Affiche la réponse aux questions et l'animation
+    addAnswer();
+    addAnimation();
+
+    // Changement de texte du bouton et désactivation de celui-ci
+    document.getElementById("sur20").innerHTML = "Vous avez " + grade + " bonnes réponses sur " + nbQuestions + "!";
+    document.getElementById("submit").innerHTML = "Merci d'avoir pris le temps de répondre";
+    document.getElementById("submit").disabled = true;
+};
+
+
+
+// Boucle pour vérifier que tout est checké
+function checking() {
+    var ifCheck = 0;
+
+    for (let i in allRadios) {
+        for (let j in allRadios[i]) {
+            if (allRadios[i][j].checked) {
+                ifCheck += 1;
+            }
+        }
+    }
+    return ifCheck;
+};
+
+
+
+// Boucle pour ajouter les réponses et pour calculer la note
+function addAnswer() {
+
+    for (let i in allRadios) {
+        for (let j in allRadios[i]) {
+            allRadios[i][j].disabled = true;
+
+            if (allRadios[i][j].checked) {
+
+                // Ajoute 1 si réponse juste
+                if (allRadios[i][j].value.substring(0, 5) == "Bonne") {
+                    allAnswer[i].classList.add("good");
+                    grade += 1;
+                }
+                allAnswer[i].innerHTML = allRadios[i][j].value;
+            }
+        }
+    }
+};
+
+
+
+//Boucle pour ajouter l'animation
+function addAnimation() {
+
+    document.getElementById("sur20").classList.add("appear");
+    for (let i in allAnswer) {
+        allAnswer[i].classList.add("appear");
+    }
+};
+
+
+
+// Boucle pour barre de progression et réinitialiser bouton
+var progressBar = 0;
+
+document.getElementById("form").onchange = function () {
+    if (checking() == nbQuestions) {
+        document.getElementById("submit").innerHTML = "Valider";
+    }
+};
+
+
+
+
+
+// Fin du Questionnaire
+
+
+
+
+
+
+
+
+
